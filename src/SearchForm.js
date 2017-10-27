0d0bc0b1f86d28ad5b5e757fcbcce045e0628a21
Jack Seabolt <jackseabolt@gmail.com>
2017-10-27 08:06:14 -0700

    get single planet functionality added

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import { connect } from 'react-redux'; 
+import { findPlanet } from './actions'; 
 import './SearchForm.css'; 
 
 export class SearchForm extends React.Component {
@@ -7,6 +8,7 @@ export class SearchForm extends React.Component {
     handleFormSubmit(e) {
         e.preventDefault(); 
         let userInput = this.input.value; 
+        this.props.dispatch(findPlanet(userInput))
         console.log(userInput)
 
     }
@@ -16,7 +18,16 @@ export class SearchForm extends React.Component {
         return (
             <section role="region">
                 <form onSubmit={e => this.handleFormSubmit(e)}>
-                    <input placeholder="Enter planet name" className="input" ref={input => this.input = input }/>
+                    <select ref={input => this.input = input }>
+                        <option value="Mercury">Mercury</option>
+                        <option value="Venus">Venus</option>
+                        <option value="Earth">Earth</option>
+                        <option value="Mars">Mars</option>
+                        <option value="Jupiter">Jupiter</option>
+                        <option value="Saturn">Saturn</option>
+                        <option value="Uranus">Uranus</option>
+                        <option value="Neptune">Neptune</option>
+                    </select>
                     <button className="button">Search</button>
                 </form>
             </section>
--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,21 @@ export const renderPlanets = planets => ({
 });
 
 export const getPlanets = () => dispatch => {
-    return fetch('https://solarsystem-api.herokuapp.com/api/planets')
+    return fetch('https://solarsystemguide.herokuapp.com/api/planets')
+        .then(res => {
+            if(!res.ok) {
+                return Promise.reject(res.statusText); 
+            }
+            return res.json(); 
+        })
+        .then(data => {
+            dispatch(renderPlanets(data))
+        }); 
+}
+
+export const FIND_PLANET = 'FIND_PLANET'; 
+export const findPlanet = userInput => dispatch => {
+    return fetch(`https://solarsystemguide.herokuapp.com/api/planets/${userInput}`)
         .then(res => {
             if(!res.ok) {
                 return Promise.reject(res.statusText); 
