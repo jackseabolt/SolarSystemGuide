fba967c13912d7d38affb5b90efa1249f69a086f
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-11-23 11:33:10 -0800

    background added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="background"></div>
+        <div class="video-background">
+        <div class="video-foreground">
+          <iframe src="https://www.youtube.com/embed/W0LHTWG-UmQ?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=W0LHTWG-UmQ" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+
         <h1>SOLAR SYSTEM GUIDE</h1>
         <SearchForm />
         <ResultsSection />
